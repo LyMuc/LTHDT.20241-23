c5b40a92b1fda219266f22f5cb2e4c58aa4fe94d
Le Thai Son 20225919 <lethaison19122004@gmail.com>
2024-12-10 21:32:59 +0700

    feature/insertion sort function

--- sourcecode/InsertionSort.java
+++ sourcecode/InsertionSort.java
@@ -13,9 +13,16 @@ public class InsertionSort extends Sort{
 	public StateSorting getStateSorting() // Phương thức để lấy về chỉ số của phần tử lớn nhất trong số các phần tử còn lại
 										  // Đây là với trường hợp của InsertionSort, còn các sort khác thì dựa trên thuật toán
 	{
+		int minn=1000000;
+		if(iSorted==iNbElement-1) bSortDone=true;
 		for(int i=iSorted; i<iNbElement; i++)
 		{
-			//... iTarget
+			//... Tim phan tu lon nhat tai moi buoc
+			if(iArray[i]<minn)
+			{
+				minn=iArray[i];
+				iTarget=i;
+			}
 		}
 		stateSorting.setiArg1(iTarget);
 		return(stateSorting);
@@ -23,13 +30,13 @@ public class InsertionSort extends Sort{
 	
 	public StateSwap getSwapSorting() // Phuong thức để lấy về chỉ số của 2 phần tử sẽ đổi chỗ tại bước này
 	{
-		stateSwap.setiArg(iSorted, iTarget);
+		stateSwap.setiArg(iSorted, iTarget); //2 số cần quan tâm tại thời điểm này
 		
 		int temp=stateSwap.getiArg2();
-		iArray[iSorted]=iArray[iTarget];
+		iArray[iSorted]=iArray[iTarget]; //Đổi chỗ 2 phần tử này
 		iArray[iTarget]=temp;
 		
-		iSorted++;
+		iSorted++; //Số phần tử đã xét tăng lên 1
 		return(stateSwap);
 	}
 }
